169792dfec8e1b22e9fbf4fcf8aa4d19877d9275
ATMartin <martin.alex.t@gmail.com>
2014-03-05 17:52:42 +0000

    Added random number generation & new route (/guesser).

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
+require 'sinatra/reloader'
 set :bind, '0.0.0.0'
 set :port, '3000'
 
+num_secret = rand(101)
+
 get '/' do
 	"Hello, World!"
+end
+
+get '/guesser' do
+	"The secret number is #{num_secret}!"
 end
