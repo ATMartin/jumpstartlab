cc740cf77beae2d9a4f121c0afd4987af587764a
ATMartin <martin.alex.t@gmail.com>
2014-03-07 18:28:02 +0000

    Added database logic.

--- idea_box/app.rb
+++ idea_box/app.rb
@@ -1,14 +1,25 @@
+require './idea.rb'
+
 class IdeaBoxApp < Sinatra::Base
 # Sinatra settings for Nitrous.IO
   set :bind, '0.0.0.0'
   set :port, 3000
-
+  configure :development do register Sinatra::Reloader end
 #------------------------- HELPERS
 
 #------------------------- ROUTING
   get '/' do 
-   #  "Hello, world!"
-   erb :index
+    erb :index, locals: {ideas: Idea.all} 
+  end
+
+  post '/' do
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save
+    redirect '/'
+  end
+
+  not_found do
+    erb :error
   end
 
   run! if app_file == $0
